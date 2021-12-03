36c18ee4af26f7d59d5d805cfb681badfcf30461
Guilherme Felipe <guilhermeempresa158@gmail.com>
2021-12-03 10:02:51 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,35 @@
+from loginn import senha, login
+def google(texto,senha):
+    """Digite o texto que será traduzido"""
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    import time
+    from random import random
+    
+
+    chrome = webdriver.Chrome(executable_path=r'chromedriver.exe')
+    chrome.get("https://educacional.fieg.com.br/")
+
+    rand = lambda:f"{random():.1f}"
+    chrome.maximize_window()
+    teste = chrome.find_element_by_xpath(r'//*[@id="username"]')
+    time.sleep(3)
+    for c in texto:
+        teste.send_keys(c)
+        time.sleep(float(rand()))
+    
+    tsl = lambda x: time.sleep(x)
+    
+    sen = chrome.find_element_by_xpath(r'//*[@id="password"]').send_keys(senha)
+    time.sleep(1)
+    chrome.find_element_by_xpath(r'//*[@id="boxForm"]/div/form/div[3]/button').click()
+    time.sleep(5)
+    chrome.find_element_by_link_text('Planejamento e Controle da Manutenção').click() #click por link tag <a>
+    tsl(2)
+    chrome.find_element_by_xpath('//*[@id="onetopictabs"]/ul[1]/li[3]/a/div').click()
+    while True:
+        time.sleep(10)
+        #chrome.close()
+
+
+google(login,senha) #login e senha
